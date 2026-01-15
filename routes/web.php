e5445e291c76b2bd11a2f07bd4b022525558a58f
Paky0u <fakhriarfiansyah88992@gmail.com>
2026-01-15 23:16:33 +0700

    eror

--- routes/web.php
+++ routes/web.php
@@ -7,18 +7,25 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\MaterialController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SubmissionController;
+use App\Http\Controllers\OTPController; 
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\OTPController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+// --- DASHBOARD ROUTE (SUDAH DIPASANG OTP) ---
 Route::get('/dashboard', [DashboardController::class, 'index'])
-    ->middleware(['auth', 'verified'])
+    ->middleware(['auth', 'verified', 'otp']) 
     ->name('dashboard');
 
+// --- OTP ROUTE ---
+Route::middleware('auth')->group(function () {
+    Route::get('/verify-otp', [OTPController::class, 'show'])->name('otp.verify');
+    Route::post('/verify-otp', [OTPController::class, 'verify'])->name('otp.verify.store');
+});
+
+// --- AUTH ROUTES LAINNYA ---
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -29,60 +36,32 @@ Route::middleware('auth')->group(function () {
     Route::post('/classes/join', [ClassController::class, 'join'])->name('classes.join');
     
     // Announcements
-    Route::post('/classes/{class}/announcements', [AnnouncementController::class, 'store'])
-        ->name('announcements.store');
-    Route::put('/classes/{class}/announcements/{announcement}', [AnnouncementController::class, 'update'])
-        ->name('announcements.update');
-    Route::delete('/classes/{class}/announcements/{announcement}', [AnnouncementController::class, 'destroy'])
-        ->name('announcements.destroy');
+    Route::post('/classes/{class}/announcements', [AnnouncementController::class, 'store'])->name('announcements.store');
+    Route::put('/classes/{class}/announcements/{announcement}', [AnnouncementController::class, 'update'])->name('announcements.update');
+    Route::delete('/classes/{class}/announcements/{announcement}', [AnnouncementController::class, 'destroy'])->name('announcements.destroy');
     
     // Materials
-    Route::get('/classes/{class}/materials', [MaterialController::class, 'index'])
-        ->name('materials.index');
-    Route::get('/classes/{class}/materials/create', [MaterialController::class, 'create'])
-        ->name('materials.create');
-    Route::post('/classes/{class}/materials', [MaterialController::class, 'store'])
-        ->name('materials.store');
-    Route::get('/classes/{class}/materials/{material}', [MaterialController::class, 'show'])
-        ->name('materials.show');
-    Route::get('/classes/{class}/materials/{material}/download', [MaterialController::class, 'download'])
-        ->name('materials.download');
-    Route::delete('/classes/{class}/materials/{material}', [MaterialController::class, 'destroy'])
-        ->name('materials.destroy');
+    Route::get('/classes/{class}/materials', [MaterialController::class, 'index'])->name('materials.index');
+    Route::get('/classes/{class}/materials/create', [MaterialController::class, 'create'])->name('materials.create');
+    Route::post('/classes/{class}/materials', [MaterialController::class, 'store'])->name('materials.store');
+    Route::get('/classes/{class}/materials/{material}', [MaterialController::class, 'show'])->name('materials.show');
+    Route::get('/classes/{class}/materials/{material}/download', [MaterialController::class, 'download'])->name('materials.download');
+    Route::delete('/classes/{class}/materials/{material}', [MaterialController::class, 'destroy'])->name('materials.destroy');
     
     // Assignments
-    Route::get('/classes/{class}/assignments', [AssignmentController::class, 'index'])
-        ->name('assignments.index');
-    Route::get('/classes/{class}/assignments/create', [AssignmentController::class, 'create'])
-        ->name('assignments.create');
-    Route::post('/classes/{class}/assignments', [AssignmentController::class, 'store'])
-        ->name('assignments.store');
-    Route::get('/classes/{class}/assignments/{assignment}', [AssignmentController::class, 'show'])
-        ->name('assignments.show');
-    Route::get('/classes/{class}/assignments/{assignment}/edit', [AssignmentController::class, 'edit'])
-        ->name('assignments.edit');
-    Route::put('/classes/{class}/assignments/{assignment}', [AssignmentController::class, 'update'])
-        ->name('assignments.update');
-    Route::delete('/classes/{class}/assignments/{assignment}', [AssignmentController::class, 'destroy'])
-        ->name('assignments.destroy');
+    Route::get('/classes/{class}/assignments', [AssignmentController::class, 'index'])->name('assignments.index');
+    Route::get('/classes/{class}/assignments/create', [AssignmentController::class, 'create'])->name('assignments.create');
+    Route::post('/classes/{class}/assignments', [AssignmentController::class, 'store'])->name('assignments.store');
+    Route::get('/classes/{class}/assignments/{assignment}', [AssignmentController::class, 'show'])->name('assignments.show');
+    Route::get('/classes/{class}/assignments/{assignment}/edit', [AssignmentController::class, 'edit'])->name('assignments.edit');
+    Route::put('/classes/{class}/assignments/{assignment}', [AssignmentController::class, 'update'])->name('assignments.update');
+    Route::delete('/classes/{class}/assignments/{assignment}', [AssignmentController::class, 'destroy'])->name('assignments.destroy');
     
     // Submissions
-    Route::post('/classes/{class}/assignments/{assignment}/submissions', [SubmissionController::class, 'store'])
-        ->name('submissions.store');
-    Route::put('/classes/{class}/assignments/{assignment}/submissions/{submission}', [SubmissionController::class, 'update'])
-        ->name('submissions.update');
-    Route::get('/classes/{class}/assignments/{assignment}/submissions/{submission}/download', [SubmissionController::class, 'download'])
-        ->name('submissions.download');
-    Route::post('/classes/{class}/assignments/{assignment}/submissions/{submission}/grade', [SubmissionController::class, 'grade'])
-        ->name('submissions.grade');
-
-    Route::middleware('auth')->group(function () {
-        Route::get('/verify-otp', [OTPController::class, 'show'])->name('otp.verify');
-        Route::post('/verify-otp', [OTPController::class, 'verify'])->name('otp.verify.store');
-
-    Route::get('/dashboard', function () { ... })
-        ->middleware(['auth', 'verified', 'otp']) // <--- Tambah 'otp'
-        ->name('dashboard');
+    Route::post('/classes/{class}/assignments/{assignment}/submissions', [SubmissionController::class, 'store'])->name('submissions.store');
+    Route::put('/classes/{class}/assignments/{assignment}/submissions/{submission}', [SubmissionController::class, 'update'])->name('submissions.update');
+    Route::get('/classes/{class}/assignments/{assignment}/submissions/{submission}/download', [SubmissionController::class, 'download'])->name('submissions.download');
+    Route::post('/classes/{class}/assignments/{assignment}/submissions/{submission}/grade', [SubmissionController::class, 'grade'])->name('submissions.grade');
 });
 
 require __DIR__.'/auth.php';
